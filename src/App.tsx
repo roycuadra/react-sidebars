5f240b460e18a5509412795b7931ccaceb9b896c
Roy Cuadra <roycuadra16@gmail.com>
2023-05-25 07:02:25 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Sidebar as Example } from "./sidebars/sidebar-7/Sidebar";
+// import { Concept as Example } from "./concepts/concept-1/Concept";
+
+function App() {
+  return <Example />;
+}
+
+export default App;
